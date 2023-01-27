08401af26c2420a140f74d937efbeb91312c73c8
linus1996 <52202324+linus1996@users.noreply.github.com>
2023-01-27 08:54:00 +0100

    Doc add code skeleton

---
+++ DECIDE/DECIDE.py
@@ -0,0 +1,103 @@
+"""
+Authors:
+
+Linus Below Blomkvist <libl@kth.se>
+ 
+code skeleton
+
+Variable names:
+
+Input variables
+
+NUMPOINTS The number of planar data points.
+
+POINTS Array containing the coordinates of data points.
+
+PARAMETERS Struct holding parameters for LIC’s (see below).
+
+LCM Logical Connector Matrix.
+
+PUV Preliminary Unlocking Vector.
+
+Output variables
+
+LAUNCH Final launch / no launch decision encoded as ”YES”, ”NO” on the standard output.
+
+In addition, the following intermediate results are computed.
+
+CMV Conditions Met Vector.
+
+PUM Preliminary Unlocking Matrix.
+
+FUV Final Unlocking Vector.
+
+"""
+
+def DECIDE():
+    return
+
+def CMV():
+    return
+
+def PUM():
+    return
+
+def FUV():
+    return
+
+def LIC_0():
+    return
+
+def LIC_1():
+    return
+
+def LIC_2():
+    return
+
+def LIC_3():
+    return
+
+def LIC_4():
+    return
+
+def LIC_5():
+    return
+
+def LIC_6():
+    return
+
+def LIC_7():
+    return
+
+def LIC_8():
+    
+    return
+
+def LIC_9():
+    
+    return
+
+def LIC_10():
+    
+    return
+
+def LIC_11():
+    
+    return
+
+def LIC_12():
+    
+    return
+
+def LIC_13():
+    
+    return
+
+def LIC_14():
+    
+    return
+
+
+
+    
+    
