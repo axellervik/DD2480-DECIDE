685706ed80c419c65dbf5fce42f229948ec8d554
Nyappy Chan <nothingklara@gmail.com>
2023-01-30 11:33:06 +0100

    feature: comments for tests for LIC0-4 and PUM

--- tests/test_DECIDE.py
+++ tests/test_DECIDE.py
@@ -4,6 +4,7 @@ from DECIDE.DECIDE import *
 
 class TestDECIDE(unittest.TestCase):
 
+    '''Test the PUM funciton with a positive and correct example'''
     def test_PUM_pos(self):
         CMV = [True, False, True, True, False, True, False, False, False, True, True, False, True, False, False]
         LCM = [
@@ -43,6 +44,7 @@ class TestDECIDE(unittest.TestCase):
         result = PUM(LCM, CMV)
         self.assertEqual(result, expected_result)
 
+    '''Test the PUM funciton with a negative example which still has the correct input format'''
     def test_PUM_neg(self):
         CMV = [True, False, True, True, False, True, False, False, False, True, True, False, True, False, False]
         LCM = [
@@ -82,96 +84,176 @@ class TestDECIDE(unittest.TestCase):
         result = PUM(LCM, CMV)
         self.assertNotEqual(result, expected_result)
 
+    '''
+    Testing a positive input case for LIC0:
+    There should exist two consecutive data points that are 
+    further apart than LENGTH1.
+    '''
     def test_LIC_0_pos(self):
         POINTS = [(1,1), (2,2), (4,4)]
         LENGTH1 = 2
         result = LIC_0(POINTS, LENGTH1)
         self.assertTrue(result)
     
+    '''
+    Testing a negative input case for LIC0:
+    There does NOT exist two consecutive data points that are 
+    further apart than LENGTH1.
+    '''
     def test_LIC_0_neg(self):
         POINTS = [(1,1), (2,2), (3,3)]
         LENGTH1 = 2
         result = LIC_0(POINTS, LENGTH1)
         self.assertFalse(result)
-        
+    
+    '''
+    Testing an input case with invalid input for LIC0:
+    Invalid input for LENGTH1, meaning LENGTH1 < 0.
+    '''
     def test_LIC_0_invalid(self):
         POINTS = [(1,1), (2,2), (3,3)]
         LENGTH1 = -2
         result = LIC_0(POINTS, LENGTH1)
         self.assertFalse(result)
 
+    '''
+    Testing a positive input case for LIC1:
+    There should exist three consecutive data points that cannot 
+    all be contained within or on a circle of radius RADIUS1.
+    '''
     def test_LIC_1_pos(self):
         POINTS = [(0,0), (0,1), (1,0)]
         RADIUS1 = 1
         self.assertTrue(LIC_1(POINTS, RADIUS1))
 
+    '''
+    Testing a negative input case for LIC1:
+    There does NOT exist three consecutive data points that 
+    cannot all be contained within or on a circle of radius RADIUS1.
+    '''
     def test_LIC_1_neg(self):
         POINTS = [(0,0), (0,1), (1,0)]
         RADIUS1 = 0.5
         self.assertFalse(LIC_1(POINTS, RADIUS1))
 
+    '''
+    Testing an input case with invalid input for LIC1:
+    Invalid input for RADIUS1, meaning RADIUS1 < 0.
+    '''
     def test_LIC_1_invalid1(self):
         POINTS = [(0,0), (0,1), (1,0)]
         RADIUS1 = -1
         self.assertFalse(LIC_1(POINTS, RADIUS1))
 
+    '''
+    Testing an input case with invalid input for LIC1:
+    Invalid input for POINTS, meaning there are less than 3 points.
+    '''
     def test_LIC_1_invalid2(self):
         POINTS = [(0,0), (0,1)]
         RADIUS1 = 1
         self.assertFalse(LIC_1(POINTS, RADIUS1))
 
+    '''
+    Testing an input case with invalid input for LIC2:
+    Invalid input for EPSILON, meaning EPSILON < 0.
+    '''
     def test_LIC_2_invalid(self):
         POINTS = [(1,1), (2,2), (3,3)]
         EPSILON = -1
         result = LIC_2(POINTS, EPSILON)
         self.assertFalse(result)
 
+    '''
+    Testing a positive input case for LIC2:
+    There does NOT exist an angle formed by three consecutive 
+    data points that meet the requirements, angle < (PI−EPSILON) 
+    and angle > (PI+EPSILON).
+    '''
     def test_LIC_2_neg(self):
         POINTS = [(2,2), (2,2), (3,3)]
         EPSILON = (m.pi/2)
         result = LIC_2(POINTS, EPSILON)
         self.assertFalse(result)
 
+    '''
+    Testing a positive input case for LIC2:
+    There should exist an angle formed by three consecutive 
+    data points that meet the requirements, angle < (PI−EPSILON) 
+    and angle > (PI+EPSILON).
+    '''
     def test_LIC_2_pos(self):
         POINTS = [(1,1), (2,2), (3,3)]
         EPSILON = (m.pi/2)
         result = LIC_2(POINTS, EPSILON)
         self.assertTrue(result)
-        
+    
+    '''
+    Testing a positive input case for LIC3:
+    There should exist three consecutive data points forming a 
+    triangle with area greater than AREA1.
+    '''
     def test_LIC_3_pos(self):
         # the following points form a triangle with area 0.5
         POINTS = [(0,0), (0,1), (1,0)]
         AREA1 = 0.49
         self.assertTrue(LIC_3(POINTS, AREA1))
 
+    '''
+    Testing a negative input case for LIC3:
+    There does NOT exist three consecutive data points forming a
+    triangle with area greater than AREA1.
+    '''
     def test_LIC_3_neg(self):
         # the following points form a triangle with area 0.5
         POINTS = [(0,0), (0,1), (1,0)]
         AREA1 = 0.5
         self.assertFalse(LIC_3(POINTS, AREA1))
-
+    
+    '''
+    Testing an input case with invalid input for LIC3:
+    Invalid input for AREA1, meaning AREA1 < 0.
+    '''
     def test_LIC_3_invalid1(self):
         POINTS = [(0,0), (0,1), (1,0)]
         AREA1 = -1
         self.assertFalse(LIC_3(POINTS, AREA1))
 
+    '''
+    Testing an input case with invalid input for LIC3:
+    Invalid input for POINTS, meaning there are less than 3 points.
+    '''
     def test_LIC_3_invalid2(self):
         POINTS = [(0,0), (0,1)]
         AREA1 = 1
         self.assertFalse(LIC_3(POINTS, AREA1))
 
+    '''
+    Testing a positive input case for LIC4:
+    There should exist at least one set of Q_PTS consecutive data 
+    points which lie in more than QUADS quadrants.
+    '''
     def test_LIC_4_pos(self):
         POINTS = [(1,1),(-1,1),(-1,-1),(1,-1)]
         Q_PTS = 4
         QUADS = 3
         self.assertTrue(LIC_4(POINTS, Q_PTS, QUADS))
 
+    '''
+    Testing a negative input case for LIC4:
+    There does NOT exist at least one set of Q_PTS consecutive 
+    data points which lie in more than QUADS quadrants.
+    '''
     def test_LIC_4_neg(self):
         POINTS = [(1,1),(2,2),(-1,-1),(1,-1)]
         Q_PTS = 2
         QUADS = 3
         self.assertFalse(LIC_4(POINTS, Q_PTS, QUADS))
 
+    '''
+    Testing an input case with invalid input for LIC4:
+    Invalid input for Q_PTS, meaning Q_PTS < 2.
+    '''
     def test_LIC_4_invalid(self):
         POINTS = [(1,1),(-1,1),(-1,-1),(1,-1)]
         Q_PTS = 1
