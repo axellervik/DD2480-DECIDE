0bd8dfcf0671452040c0a72ba36bf43db8f5bc94
asbai <asbai@kth.se>
2023-01-30 11:35:08 +0100

    doc added comment on top of  tests for LIC 14 - 5

--- tests/test_DECIDE.py
+++ tests/test_DECIDE.py
@@ -178,63 +178,107 @@ class TestDECIDE(unittest.TestCase):
         QUADS = 1
         self.assertFalse(LIC_4(POINTS, Q_PTS, QUADS))
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """ 
     def test_LIC_5_pos(self):
         POINTS = [(1,1), (1,2), (3,3)]
         result = LIC_5(POINTS)
         self.assertTrue(result)
-        
+
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """    
     def test_LIC_5_neg(self):
         POINTS = [(1,1), (2,2), (3,3)]
         result = LIC_5(POINTS)
         self.assertFalse(result)
-        
+
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """        
     def test_LIC_5_invalid(self):
         POINTS = [(1,1)]
         result = LIC_5(POINTS)
         self.assertFalse(result)
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """ 
     def test_LIC_6_pos_1(self):
         DIST = 1
         N_PTS = 3
         POINTS = [(0,0),(1,2),(2,0),(5,5)] 
         self.assertTrue(LIC_6(POINTS, N_PTS, DIST))
-        
+
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """         
     def test_LIC_6_pos_2(self):
         DIST = 1
         N_PTS = 3
         POINTS = [(0,0),(1,2),(0,0),(5,5)] 
         self.assertTrue(LIC_6(POINTS, N_PTS, DIST))
-    
+
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """  
     def test_LIC_6_neg(self):
         DIST = 10
         N_PTS = 3
         POINTS = [(0,0),(1,2),(2,0),(5,5)] 
         self.assertFalse(LIC_6(POINTS, N_PTS, DIST))    
-            
+
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable N_PTS is less that 3.
+    """        
     def test_LIC_6_invalid(self):
         DIST = 2
         N_PTS = 2
         POINTS = [(1,1),(2,2)]    
         self.assertFalse(LIC_6(POINTS, N_PTS, DIST))
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """ 
     def test_LIC_7_pos(self):
         POINTS = [(0,0),(5,5),(99,-99),(10,10)]
         K_POINTS = 2
         LENGTH1 = 10
         self.assertTrue(LIC_7(POINTS, K_POINTS, LENGTH1))
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """ 
     def test_LIC_7_neg(self):
         POINTS = [(0,0),(100,100),(2,2),(102,102)]
         K_POINTS = 1
         LENGTH1 = 100
         self.assertFalse(LIC_7(POINTS, K_POINTS, LENGTH1))
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the given length is too short.
+    """
     def test_LIC_7_invalid(self):
         POINTS = [(0,0),(1,1)]
         K_POINTS = 1
         LENGTH1 = -1
         self.assertFalse(LIC_7(POINTS, K_POINTS, LENGTH1))
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable A_PTS is less than 1.
+    """
     def test_LIC_8_invalid(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -244,6 +288,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_8(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable B_PTS is less than 1.
+    """
     def test_LIC_8_invalid2(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -253,6 +301,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_8(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variables A_PTS and B_PTS added together is more the the number of datapoints - 3.
+    """
     def test_LIC_8_invalid3(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -262,6 +314,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_8(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """   
     def test_LIC_8_neg(self):
         NUMPOINTS = 3
         POINTS = [(1,1), (2,2), (3,3)]
@@ -271,6 +327,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_8(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """   
     def test_LIC_8_neg2(self):
         NUMPOINTS = 6
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6)]
@@ -280,6 +340,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_8(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1)
         self.assertFalse(result)
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """   
     def test_LIC_8_pos(self):
         NUMPOINTS = 6
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6)]
@@ -289,6 +353,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_8(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1)
         self.assertTrue(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable C_PTS is less than 1.
+    """
     def test_LIC_9_invalid(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -298,6 +366,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_9(NUMPOINTS, POINTS, C_PTS, D_PTS, EPSILON)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable D_PTS is less than 1.
+    """
     def test_LIC_9_invalid2(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -307,6 +379,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_9(NUMPOINTS, POINTS, C_PTS, D_PTS, EPSILON)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variables C_PTS and D_PTS added together is more the the number of datapoints - 3.
+    """
     def test_LIC_9_invalid3(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -316,6 +392,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_9(NUMPOINTS, POINTS, C_PTS, D_PTS, EPSILON)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """
     def test_LIC_9_neg(self):
         NUMPOINTS = 4
         POINTS = [(1,1), (2,2), (3,3), (4,4)]
@@ -325,6 +405,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_9(NUMPOINTS, POINTS, C_PTS, D_PTS, EPSILON)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """
     def test_LIC_9_neg2(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (3,1), (3,3), (4,4), (5,4)]
@@ -333,7 +417,11 @@ class TestDECIDE(unittest.TestCase):
         EPSILON = m.pi - m.pi / 100
         result = LIC_9(NUMPOINTS, POINTS, C_PTS, D_PTS, EPSILON)
         self.assertFalse(result)
-    
+
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """    
     def test_LIC_9_pos(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (3,2), (3,3), (4,4), (5,5)]
@@ -343,6 +431,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_9(NUMPOINTS, POINTS, C_PTS, D_PTS, EPSILON)
         self.assertTrue(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable E_PTS is less than 1.
+    """
     def test_LIC_10_invalid(self):
         #Checks if E_PTS => 1
         NUMPOINTS = 5
@@ -353,8 +445,11 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_10(NUMPOINTS, POINTS, E_PTS, F_PTS, AREA1)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable F_PTS is less than 1.
+    """
     def test_LIC_10_invalid2(self):
-        #Checks if F_PTS => 1
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
         E_PTS = 1
@@ -363,8 +458,11 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_10(NUMPOINTS, POINTS, E_PTS, F_PTS, AREA1)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variables E_PTS and F_PTS added together is more the the number of datapoints - 3.
+    """
     def test_LIC_10_invalid3(self):
-        #Checks if E_PTS + F_PTS <= NUMPPOINTS - 3
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
         E_PTS = 2
@@ -373,8 +471,11 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_10(NUMPOINTS, POINTS, E_PTS, F_PTS, AREA1)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the number of datapoints is less too low.
+    """
     def test_LIC_10_neg(self):
-        #Checks if NUMPOINTS => 5
         NUMPOINTS = 4
         POINTS = [(1,1), (2,2), (3,3), (4,4)]
         E_PTS = 1
@@ -383,8 +484,11 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_10(NUMPOINTS, POINTS, E_PTS, F_PTS, AREA1)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """
     def test_LIC_10_neg2(self):
-        #Checks a negative case of points
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (3,1)]
         E_PTS = 2
@@ -393,8 +497,11 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_10(NUMPOINTS, POINTS, E_PTS, F_PTS, AREA1)
         self.assertFalse(result)
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """
     def test_LIC_10_pos(self):
-        #Checks a positive case of points
         NUMPOINTS = 5
         POINTS = [(1,1), (3,3), (3,1), (4,4), (5,5)]
         E_PTS = 1
@@ -403,22 +510,32 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_10(NUMPOINTS, POINTS, E_PTS, F_PTS, AREA1)
         self.assertTrue(result)
 
-    def test_LIC_11_invalid1(self):
-        # tests is False if G_PTS > NUMPOINTS - 2 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that tthe variable G_PTS is more than the number of datapoints - 2.
+    """
+    def test_LIC_11_invalid(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
         G_PTS = 4
         result = LIC_11(NUMPOINTS, POINTS, G_PTS)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the variable G_PTS is less than 1.
+    """
     def test_LIC_11_invalid2(self):
-        # tests is False if G_PTS < 1
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
         G_PTS = 0
         result = LIC_11(NUMPOINTS, POINTS, G_PTS)
         self.assertFalse(result)
-        
+
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """    
     def test_LIC_11_pos(self):
         NUMPOINTS = 5
         POINTS = [(2,2), (3,3), (1,1), (4,4), (5,5)]
@@ -426,14 +543,21 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_11(NUMPOINTS, POINTS, G_PTS)
         self.assertTrue(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """
     def test_LIC_11_neg(self):
-        # tests is False if NUMPOINTS <= 3
         NUMPOINTS = 2
         POINTS = [(1,1), (2,2)]
         G_PTS = 0
         result = LIC_11(NUMPOINTS, POINTS, G_PTS)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the given length is less than zero.
+    """
     def test_LIC_12_invalid(self):
         NUMPOINTS = 3
         POINTS = [(1,1), (2,2), (3,3)]
@@ -443,6 +567,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_12(NUMPOINTS, POINTS, LENGTH1, LENGTH2, K_PTS)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """
     def test_LIC_12_neg(self):
         NUMPOINTS = 1
         POINTS = [(1,1)]
@@ -452,6 +580,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_12(NUMPOINTS, POINTS, LENGTH1, LENGTH2, K_PTS)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """
     def test_LIC_12_neg2(self):
         NUMPOINTS = 3
         POINTS = [(1,1), (2,2), (3,3)]
@@ -461,6 +593,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_12(NUMPOINTS, POINTS, LENGTH1, LENGTH2, K_PTS)
         self.assertFalse(result)
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """
     def test_LIC_12_pos(self):
         NUMPOINTS = 3
         POINTS = [(1,1), (2,2), (3,3)]
@@ -470,6 +606,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_12(NUMPOINTS, POINTS, LENGTH1, LENGTH2, K_PTS)
         self.assertTrue(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the given radius is less than zero.
+    """
     def test_LIC_13_invalid(self):
         NUMPOINTS = 5
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5)]
@@ -480,6 +620,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_13(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1, RADIUS2)
         self.assertFalse(result)
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """
     def test_LIC_13_pos(self):
         NUMPOINTS = 6
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6)]
@@ -490,6 +634,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_13(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1, RADIUS2)
         self.assertTrue(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """
     def test_LIC_13_neg(self):
         NUMPOINTS = 3
         POINTS = [(1,1), (2,2), (3,3)]
@@ -500,6 +648,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_13(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1, RADIUS2)
         self.assertFalse(result)
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """
     def test_LIC_13_neg2(self):
         NUMPOINTS = 6
         POINTS = [(1,1), (2,2), (3,3), (4,4), (5,5), (6,6)]
@@ -510,6 +662,10 @@ class TestDECIDE(unittest.TestCase):
         result = LIC_13(NUMPOINTS, POINTS, A_PTS, B_PTS, RADIUS1, RADIUS2)
         self.assertFalse(result)
 
+    """
+    Invalid test: 
+    Checks wether the method recognizes that the given area is less than zero.
+    """
     def test_LIC_14_invalid(self):
         AREA1 = 1
         AREA2 = -1
@@ -519,6 +675,10 @@ class TestDECIDE(unittest.TestCase):
         NUMPOINTS = 5
         self.assertFalse(LIC_14(POINTS, NUMPOINTS, AREA1, AREA2, E_PTS, F_PTS))
 
+    """
+    Positive test: 
+    Checks wether the method recognizes that the set of datapoints meet the requirements of the LIC.
+    """ 
     def test_LIC_14_pos(self):
         AREA1 = 1
         AREA2 = 100
@@ -528,8 +688,11 @@ class TestDECIDE(unittest.TestCase):
         NUMPOINTS = 6
         self.assertTrue(LIC_14(POINTS, NUMPOINTS, AREA1, AREA2, E_PTS, F_PTS))
 
+    """
+    Negative test:
+    Checks wether the method recognizes that the number of datapoints is too low.
+    """
     def test_LIC_14_neg(self):
-        # tests if False if numpoints is less than 5
         AREA1 = 1
         AREA2 = 1
         E_PTS = 1
@@ -538,8 +701,11 @@ class TestDECIDE(unittest.TestCase):
         NUMPOINTS = 3
         self.assertFalse(LIC_14(POINTS, NUMPOINTS, AREA1, AREA2, E_PTS, F_PTS))
 
+    """
+    Negative test: 
+    Checks wether the method recognizes that the set of datapoints don't meet the requirements of the LIC.
+    """
     def test_LIC_14_neg_2(self):
-        # tests if False if wrong areas
         AREA1 = 100
         AREA2 = 1
         E_PTS = 1
